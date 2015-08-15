a1148945f883a340565ae851810571d26c964faa
Fabien Arcellier <farcellier@octo.com>
2015-08-15 10:30:00 +0000

    SNIP | USE CLICK TO MATCH COMMAND LINE ARGUMENTS
    use click to implement command line command capture (save, list, default)

--- snip.py
+++ snip.py
@@ -1,22 +1,33 @@
 #!/usr/bin/python
+#pylint: disable=missing-docstring
 
-import sys
+import click
 
-def main(args):
+@click.group(invoke_without_command=True)
+@click.argument('snippet')
+def cli(snippet):
+    # case : default
+    # snip python-test
+    # Copy the content of python-test in clipboard
+    click.echo('get snippet {0}'.format(snippet))
+
+@click.command()
+@click.argument('snippet')
+def save(snippet):
     # case : save
     # snip save python-test
     # Create a file python-test in ~/.snip
     # that contains the content of clip board
+    click.echo('save snippet {0}'.format(snippet))
 
+@click.command(name='list')
+@click.argument('snippetpattern')
+def cli_list(snippetpattern):
     # case : list
     # snip list python
     # Get a list of all files that begins with
     # python
+    click.echo('list snippet {0}'.format(snippetpattern))
 
-    # case : default
-    # snip python-test
-    # Copy the content of python-test in clipboard
-    pass
-
-if __name__ == "__main__":
-    main(sys.argv[1:])
+cli.add_command(save)
+cli.add_command(list)
--- snip_test.py
+++ snip_test.py
@@ -1,10 +1,13 @@
-import snip
+#!/usr/bin/python
+#pylint: disable=missing-docstring
+#pylint: disable=too-many-public-methods
+
 import unittest
 
 class SnipTest(unittest.TestCase):
     def setUp(self):
         pass
-    
+
     def test_test1(self):
         # Assign
         # Acts
